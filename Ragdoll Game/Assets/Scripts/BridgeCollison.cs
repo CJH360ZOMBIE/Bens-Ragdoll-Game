2e21ecfca7117996b839c726c32e35df1dbc30a9
CJH360ZOMBIE <charliejhargreaves@gmail.com>
2020-08-21 18:56:28 +0100

    2

---
+++ Ragdoll Game/Assets/Scripts/BridgeCollison.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+public class BridgeCollison : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.tag ==("Ground")) 
+        {
+            CameraShaker.Instance.ShakeOnce(10f, 10f, .1f, .1f);
+        } 
+    }
+}
